dd02db1c9ebd1ab3dd97c1bca096b377c8c8b834
blipn <oblipn@gmail.com>
2016-11-14 01:18:29 +0100

    Changed name

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-#LiveBox-PC-Remote
+#LiveBoxTV-PC-Remote
 
 ![Image of Yaktocat](https://github.com/blipn/LiveBox-PC-Remote/blob/master/Capture.PNG)
 
-LiveBox PC Remote est une télécommande pour le décodeur livebox play de Orange.
+LiveBoxTV PC Remote est une télécommande pour le décodeur livebox play de Orange.
--- src/LBPCR/Main.java
+++ src/LBPCR/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Main-Window.fxml"));
-        primaryStage.setTitle("LiveBox PC Remote");
+        primaryStage.setTitle("LiveBoxTV PC Remote");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
